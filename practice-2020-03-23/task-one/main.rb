31502796cf57f955fedc8e490a67fbbf23e11ad2
Andrey <andrey.vasilyev@fruct.org>
2020-03-23 14:55:19 +0300

    Added stub implementation of MyComplex

---
+++ practice-2020-03-23/task-one/main.rb
@@ -0,0 +1,7 @@
+require_relative 'my_complex'
+
+complex_one = MyComplex.new
+complex_two = MyComplex.new
+
+pp complex_one
+pp complex_two
---
+++ practice-2020-03-23/task-one/my_complex.rb
@@ -0,0 +1,7 @@
+class MyComplex
+
+end
+
+if __FILE__ == $0
+  
+end
